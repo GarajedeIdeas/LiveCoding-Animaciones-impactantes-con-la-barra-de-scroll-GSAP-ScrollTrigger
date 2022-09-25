6401403b26332cc3a0efe937ca754e0fbfa7e4a1
Dani Vicario <danigv.email@gmail.com>
2022-09-25 23:25:13 +0200

    feat: added timeline

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,42 @@
 import "./styles.scss";
 
-import { gsap } from "gsap";
+import { Quad, gsap } from "gsap";
 
-gsap.to(".a", {
-  rotate: 180,
-  x: 400,
-  delay: 0
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.defaults({
+  duration: 2
+});
+
+const tl = gsap
+  .timeline({ paused: !true })
+  .to(".a", {
+    xPercent: -100,
+    backgroundColor: "yellow",
+    ease: Quad.easeIn
+  })
+  .to(".b", {
+    xPercent: -100
+    // duration: 4
+  });
+// .to(".c", {
+//   xPercent: -100
+// });
+
+const containerDOMEl: HTMLElement =
+  document.querySelector("#container")!;
+
+const height2 = containerDOMEl.offsetHeight * 10;
+
+console.log(height2);
+
+ScrollTrigger.create({
+  animation: tl,
+  start: "top top",
+  end: () => "+=" + height2,
+  trigger: "#container",
+  scrub: true,
+  pin: !true
 });
