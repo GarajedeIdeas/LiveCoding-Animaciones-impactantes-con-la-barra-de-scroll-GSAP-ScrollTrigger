d87c8a065a891ced8cccf949a50e4b1579405f78
Dani Vicario <danigv.email@gmail.com>
2022-09-26 00:21:29 +0200

    feat: example 2

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,31 @@
 import "./styles.scss";
 
-import { Quad, gsap } from "gsap";
-
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { gsap } from "gsap";
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.defaults({
-  duration: 2
-});
-
 const tl = gsap
-  .timeline({ paused: !true })
-  .to(".a", {
-    xPercent: -100,
-    backgroundColor: "yellow",
-    ease: Quad.easeIn
+  .timeline()
+  .to(".box.a", {
+    xPercent: 100,
+    duration: 5
   })
-  .to(".b", {
-    xPercent: -100
-    // duration: 4
+  .to(".box.b", {
+    yPercent: 100,
+    duration: 15
+  })
+  .to(".box.c", {
+    backgroundColor: "orange",
+    duration: 15
   });
-// .to(".c", {
-//   xPercent: -100
-// });
-
-const containerDOMEl: HTMLElement =
-  document.querySelector("#container")!;
-
-const height2 = containerDOMEl.offsetHeight * 10;
-
-console.log(height2);
 
 ScrollTrigger.create({
+  // markers: true,
   animation: tl,
-  start: "top top",
-  end: () => "+=" + height2,
   trigger: "#container",
-  scrub: true,
-  pin: true
+  start: "top top",
+  end: "+=5000",
+  pin: true,
+  scrub: true
 });
