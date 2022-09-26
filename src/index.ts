7ce76b4ab3960e5ac336d6bf3d7bce7c1f4d4f8f
Dani Vicario <danigv.email@gmail.com>
2022-09-26 23:56:43 +0200

    example 2

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,44 @@
 import "./styles.scss";
 
+import { Power4, gsap } from "gsap";
+
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { gsap } from "gsap";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const tl = gsap
   .timeline()
+  .timeScale(8)
+  .addLabel("l1")
   .to(".box.a", {
     xPercent: 100,
     duration: 5
   })
+  .addLabel("l2")
   .to(".box.b", {
     yPercent: 100,
-    duration: 15
+    rotation: 20,
+    duration: 15,
+    ease: Power4.easeOut
   })
+  .to(".box.b", {
+    opacity: 0,
+    duration: 5
+  })
+  .addLabel("l3")
   .to(".box.c", {
     backgroundColor: "orange",
     duration: 15
   });
+
+tl.seek("l2");
 
 ScrollTrigger.create({
   // markers: true,
   animation: tl,
   trigger: "#container",
   start: "top top",
-  end: "+=5000",
+  end: "+=50000",
   pin: true,
   scrub: true
 });
