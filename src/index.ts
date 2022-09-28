f65f2b9dc4c593c179c5252e3de27d950965e6cb
Dani Vicario <danigv.email@gmail.com>
2022-09-28 16:45:46 +0200

    boxes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "./styles.scss";
 
-import gsap from "gsap";
+import gsap, { Elastic } from "gsap";
 
 const w = window.innerWidth;
 const h = window.innerHeight;
@@ -16,78 +16,54 @@ function randomInt(min: number, max: number) {
   );
 }
 
-const createCircle = (
-  x: number,
-  y: number,
-  r: number
-) => {
-  const circle = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "circle"
-  );
+const { body } = document;
+const padding = 110;
 
-  gsap.set(circle, {
-    attr: {
-      cx: x.toString(),
-      cy: y.toString(),
-      r: r.toString()
-    }
-  });
+for (
+  let x = 0;
+  x < window.innerWidth;
+  x += padding
+) {
+  for (
+    let y = 0;
+    y < window.innerHeight;
+    y += padding
+  ) {
+    const sqDOMEl = document.createElement("div");
+    sqDOMEl.classList.add("box");
 
-  gsap
-    .timeline()
-    .to(circle, {
-      fill: `rgba(${randomInt(
-        0,
-        255
-      )}, ${randomInt(0, 255)}, ${randomInt(
-        0,
-        255
-      )}, 1)`
-    })
-    .to(circle, {
-      delay: randomFloat(1, 5),
-      r: 0,
-      yoyo: true,
-      repeat: -1
+    gsap.set(sqDOMEl, {
+      scale: 1, //randomFloat(0.15, 1),
+      x,
+      y
     });
 
-  return circle;
-};
-
-const { body } = document;
-
-const svgDOMEl = document.createElementNS(
-  "http://www.w3.org/2000/svg",
-  "svg"
-);
-body.appendChild(svgDOMEl);
-
-gsap.set(svgDOMEl, {
-  attr: {
-    height: h,
-    width: w
-  }
-});
-
-function createCircles(
-  rCircle: number,
-  r: number,
-  total: number
-) {
-  for (let i = 0; i < 360; i += 360 / total) {
-    svgDOMEl.appendChild(
-      createCircle(
-        w2 +
-          rCircle * Math.cos((i * Math.PI) / 180),
-        h2 +
-          rCircle * Math.sin((i * Math.PI) / 180),
-        r
-      )
-    );
+    body.appendChild(sqDOMEl);
   }
 }
 
-createCircles(100, 10, 10);
-createCircles(200, 20, 30);
-createCircles(300, 30, 50);
+const boxes = gsap.utils.toArray(".box");
+
+boxes.forEach((box: any, index) =>
+  gsap.set(box, {
+    background: () => {
+      const hsl = (boxes.length / index) * 360;
+      return "hsl(" + hsl + ", 100%, 50%)";
+    }
+  })
+);
+
+gsap.to(".box", {
+  delay: 0.2,
+  duration: 5,
+  rotation: 180,
+  ease: Elastic.easeOut,
+  background: "red",
+  stagger: {
+    // repeat: -1,
+    // yoyo: true,
+    amount: 4,
+    from: "edges",
+    grid: "auto"
+  }
+});
