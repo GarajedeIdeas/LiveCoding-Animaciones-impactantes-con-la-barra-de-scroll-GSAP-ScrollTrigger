7d1e5ebacb07f498e50989eb14a56e7e002781bd
Dani Vicario <danivicario@dvicario.es>
2022-09-29 09:23:45 +0200

    final

--- src/index.ts
+++ src/index.ts
@@ -6,39 +6,30 @@ import { gsap } from "gsap";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const t1 = gsap.to(".box.b", {
-  opacity: 0.5,
-  duration: 1,
-  onReverseComplete: () => {
-    if (Math.random() > 0.5) {
-      t1.resetTo("opacity", 0);
-    } else {
-      t1.resetTo("opacity", 1);
-    }
-  }
-});
-
 const tl = gsap
   .timeline()
   .to(".box.a", {
     top: "-100vh",
-    delay: 1,
     duration: 1
   })
-  .add(t1)
+  .to(".box.b", {
+    left: "100vw",
+    duration: 2,
+    // scale: 2
+    rotation: 180
+  })
   .to(".box.c", {
-    // yPercent: 100,
-
-    opacity: 0,
-    // ease: Elastic.easeIn,
-    duration: 1
+    // left: "100vw",
+    duration: 1,
+    opacity: 0
   });
 
 ScrollTrigger.create({
   animation: tl,
   trigger: "#container",
   start: "top top",
-  end: `+=${1205 * 5}`,
+  end: "+=20000",
+  scrub: true,
   pin: true,
-  scrub: true
+  markers: true
 });
